2e1fb92397df93988fb2f984377956ead904f4a1
Murilo Marinho <murilo@sas-ide-VM>
2022-07-04 17:04:03 +0900

    [sas_common.cpp] Tested, removing prints.

--- src/sas_common.cpp
+++ src/sas_common.cpp
@@ -1,3 +1,26 @@
+/*
+# Copyright (c) 2022 Murilo Marques Marinho
+#
+#    This file is part of sas_common.
+#
+#    sas_common is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Lesser General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    sas_common is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Lesser General Public License for more details.
+#
+#    You should have received a copy of the GNU Lesser General Public License
+#    along with sas_common.  If not, see <https://www.gnu.org/licenses/>.
+#
+# ################################################################
+#
+#   Author: Murilo M. Marinho, email: murilo@g.ecc.u-tokyo.ac.jp
+#
+# ################################################################*/
 #include <sas_common/sas_common.h>
 #include <mutex>
 
@@ -16,18 +39,21 @@ ros::NodeHandle& get_static_node_handle()
     std::lock_guard<std::mutex> lock(node_handle_mutex);
     if(!ros::isInitialized())
     {
-        ROS_INFO_STREAM(ros::this_node::getName()+"::Initializing roscpp with a dummy name...");
+        //ROS_INFO_STREAM("sas_common::Initializing roscpp.");
         int argc = 0;
         char** argv = nullptr;
         ros::init(argc,argv,"sas_common_nodehandle_dummy", ros::init_options::AnonymousName | ros::init_options::NoSigintHandler);
     }
     if(not node_handle)
     {
+        //ROS_INFO_STREAM("sas_common::Initializing ros::NodeHandle.");
         node_handle = {new ros::NodeHandle(),
                        [](ros::NodeHandle* nh)
                        {
+                           //std::cout << "sas_common::Shutdown roscpp."<< std::endl;
                            if (ros::isInitialized() && !ros::isShuttingDown())
                            {
+                               async_spinner->stop();
                                ros::shutdown();
                            }
                            if(nh != nullptr)
@@ -36,8 +62,8 @@ ros::NodeHandle& get_static_node_handle()
                            }
                        }
                       };
-       async_spinner.reset(new ros::AsyncSpinner(1));
-       async_spinner->start();
+        async_spinner.reset(new ros::AsyncSpinner(1));
+        async_spinner->start();
     }
     return (*node_handle.get());
 }
